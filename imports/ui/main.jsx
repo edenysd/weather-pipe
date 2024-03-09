043c4e9aa0d66d6ed64ffe1637586d7a5a547be2
Edenys Deniz González <holofist1@gmail.com>
2024-03-08 21:59:02 -0500

    fix: config router

--- imports/ui/main.jsx
+++ imports/ui/main.jsx
@@ -2,7 +2,8 @@
 import { Meteor } from "meteor/meteor";
 import { render } from "solid-js/web";
 
-import { lazy } from "solid-js";
+import Cookies from "js-cookie";
+import { createSignal } from "solid-js";
 import { Login } from "./app/Login";
 import { Router } from "@solidjs/router";
 
@@ -13,14 +14,22 @@ const routes = [
   },
 ];
 
-export const App = () => {
+const COOKIE_ID = "lg-user-cookie";
+
+export const App = (props) => {
+  const [token, setToken] = createSignal(Cookies.get(COOKIE_ID));
+
   return (
-    <div>
-      <Router>{routes}</Router>
-    </div>
+    <>
+      Adasasd
+      <div>{props.children}</div>
+    </>
   );
 };
 
 Meteor.startup(() => {
-  render(() => <App />, document.getElementById("root"));
+  render(
+    () => <Router root={App}>{routes}</Router>,
+    document.getElementById("root")
+  );
 });
