8a909aa3a9a57d20b44e305374d68b46c595a218
Edenys Deniz González <holofist1@gmail.com>
2024-03-10 04:04:55 -0400

    feat: replace svgs with icon components

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -3,7 +3,7 @@ import { onMount } from "solid-js";
 import * as Toast from "~/components/toast";
 import { tryLogInWithCookies } from "./lib/auth";
 import { useNavigate } from "@solidjs/router";
-
+import { FaSolidXmark } from "solid-icons/fa";
 /**
  * @important
  * This invocation cause some warnings in SolidJS
@@ -19,18 +19,7 @@ export const [Toaster, toast] = createToaster({
         <Toast.Title class="mb-2">{toast().title}</Toast.Title>
         <Toast.Description>{toast().description}</Toast.Description>
         <Toast.CloseTrigger>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <FaSolidXmark />
         </Toast.CloseTrigger>
       </Toast.Root>
     );
