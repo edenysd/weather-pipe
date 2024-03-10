ee44638168fbc1b8b97efbd7d98fb1debf41d943
Edenys Deniz González <holofist1@gmail.com>
2024-03-09 23:09:04 -0500

    feat: add auth module

--- imports/ui/lib/auth.tsx
+++ imports/ui/lib/auth.tsx
@@ -1,5 +1,35 @@
-export type AuthInfo = { name: string; password: string };
+const { Accounts } = require("meteor/accounts-base");
+import { Meteor } from "meteor/meteor";
 
-export const handleAuth = (authInfo: AuthInfo) => {
-  console.log(authInfo);
+export type AuthInfo = { username: string; password: string };
+
+export const signUp = async (authInfo: AuthInfo) => {
+  try {
+    await Accounts.createUserAsync(authInfo);
+    return;
+  } catch (error) {
+    throw {
+      reason: error.reason,
+      error: error.error,
+    };
+  }
 };
+
+export const logIn = async (authInfo: AuthInfo) => {
+  Meteor.loginWithPassword(
+    { username: authInfo.username },
+    authInfo.password,
+    (error) => {
+      if (!error) {
+        //@todo LOGIC for saved keys
+      } else {
+        return {
+          reason: error.reason,
+          error: error.error,
+        };
+      }
+    }
+  );
+};
+
+export const tryLogInWithCookies = () => {};
