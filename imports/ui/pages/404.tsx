e3859dcd199ce00abc0f36396af862018e26aa4e
Edenys Deniz González <holofist1@gmail.com>
2024-03-10 01:42:16 -0500

    feat: add info and actions to not found path

---
+++ imports/ui/pages/404.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "@solidjs/router";
+import { onMount } from "solid-js";
+import { Button } from "~/components/button";
+
+export const NotFound = () => {
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/home");
+  };
+
+  onMount(() => {
+    navigate("/404");
+  });
+  return (
+    <section class="flex flex-col items-center justify-center absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+      <h1 class="text-7xl text-black font-thin">Not Found</h1>
+      <h1 class="text-5xl text-black mb-10">404</h1>
+      <Button size="xl" color="black" onClick={goHome}>
+        Go Home
+      </Button>
+    </section>
+  );
+};
