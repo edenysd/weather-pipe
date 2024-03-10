c5bec00025373f4c5619dbce4345c48ced724437
Edenys Deniz González <holofist1@gmail.com>
2024-03-10 16:28:12 -0400

    feat: configure flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 const { parkwindPlugin } = require("@park-ui/tailwind-plugin");
+const flowbite = require("flowbite/plugin");
 
 module.exports = {
-  content: ["./client/**/*.{js,jsx,ts,tsx}", "./imports/**/*.{js,jsx,ts,tsx}"],
-  plugins: [parkwindPlugin],
+  content: [
+    "./client/**/*.{js,jsx,ts,tsx}",
+    "./imports/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  plugins: [parkwindPlugin, flowbite],
   theme: {
     extend: {},
   },
