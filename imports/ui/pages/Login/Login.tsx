370c51c5f57b4039b82e8826521918a5494e471e
Edenys Deniz González <holofist1@gmail.com>
2024-03-11 15:58:13 -0400

    fix: add z-index to sections

--- imports/ui/pages/Login/Login.tsx
+++ imports/ui/pages/Login/Login.tsx
@@ -4,13 +4,13 @@ import { WiDust } from "solid-icons/wi";
 export const Login = () => {
   return (
     <section class="flex flex-col lg:flex-row lg:justify-evenly gap-5 items-center justify-center absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
-      <div class="flex flex-col items-center justify-center">
+      <div class="flex flex-col items-center justify-center z-20">
         <WiDust class="text-9xl" color="white" />
         <h1 class="text-4xl md:text-7xl -translate-y-4 text-white">
           Weather Pipe
         </h1>
       </div>
-      <div class="p-3 md:p-0 lg:w-1/3">
+      <div class="p-3 md:p-0 lg:w-1/3 z-20">
         <SignUpCard />
       </div>
     </section>
