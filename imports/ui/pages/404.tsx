ef0d709bc7d0f551e415fe7e8b94dbd3bd9dec80
Edenys Deniz González <holofist1@gmail.com>
2024-03-11 15:57:34 -0400

    fix: add z-index

--- imports/ui/pages/404.tsx
+++ imports/ui/pages/404.tsx
@@ -16,7 +16,7 @@ export const NotFound = () => {
     <section class="flex flex-col items-center justify-center absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
       <h1 class="text-7xl text-black font-thin">Not Found</h1>
       <h1 class="text-5xl text-black mb-10">404</h1>
-      <Button size="xl" color="black" onClick={goHome}>
+      <Button class="z-2" size="xl" color="black" onClick={goHome}>
         Go Home
       </Button>
     </section>
