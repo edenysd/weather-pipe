ee2817910da167530ec206b79f0ec45664a6815a
Edenys Deniz González <holofist1@gmail.com>
2024-03-10 22:54:34 -0400

    feat: configure home layout route ward

--- imports/ui/pages/Home/HomeLayout.tsx
+++ imports/ui/pages/Home/HomeLayout.tsx
@@ -1,7 +1,26 @@
+import { createMemo, onMount } from "solid-js";
 import { NavBar } from "./NavBar";
 import { SideBar } from "./SideBar";
+import { useLocation, useNavigate } from "@solidjs/router";
+import { Meteor } from "meteor/meteor";
 
 export const HomeLayout = (props) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const pathname = createMemo(() => location.pathname);
+
+  onMount(async () => {
+    if (!Meteor.user()) {
+      navigate("/");
+      return;
+    }
+    //Redirect
+    if (pathname() === "/home") {
+      navigate("dashboard");
+    }
+  });
+
   return (
     <>
       <NavBar />
