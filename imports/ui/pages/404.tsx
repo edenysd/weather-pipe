b55f6bbdc8e647eb383c26807646ac5d8287cec0
Edenys Deniz González <holofist1@gmail.com>
2024-03-11 17:03:08 -0400

    fix: remove negative z-index to avoid the necesity of positive ones

--- imports/ui/pages/404.tsx
+++ imports/ui/pages/404.tsx
@@ -13,10 +13,10 @@ export const NotFound = () => {
     navigate("/404");
   });
   return (
-    <section class="flex flex-col items-center justify-center absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+    <section class="flex flex-col items-center justify-center absolute top-0 h-screen w-screen bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
       <h1 class="text-7xl text-black font-thin">Not Found</h1>
       <h1 class="text-5xl text-black mb-10">404</h1>
-      <Button class="z-2" size="xl" color="black" onClick={goHome}>
+      <Button size="xl" color="black" onClick={goHome}>
         Go Home
       </Button>
     </section>
