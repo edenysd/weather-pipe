2b4647dbb15e84f9137593dadbc6e67f78317912
Edenys Deniz González <holofist1@gmail.com>
2024-03-12 01:42:46 -0400

    feat: add map proxy call

---
+++ imports/api/methods/map.ts
@@ -0,0 +1,43 @@
+import { check } from "meteor/check";
+import axios from "axios";
+let i = 0;
+export const startMethodMap = () => {
+  Meteor.methods({
+    async map({ tileCoord }) {
+      if (!this.userId) {
+        throw new Meteor.Error(
+          "not-permited",
+          "Please log in to call this method"
+        );
+      }
+      console.log(i++);
+      if (tileCoord?.length !== 3) {
+        throw new Meteor.Error(
+          "validation-error",
+          "tilecoord must be an array with 3 elements"
+        );
+      }
+      check(tileCoord[0], Number);
+      check(tileCoord[1], Number);
+      check(tileCoord[2], Number);
+      this.unblock();
+
+      try {
+        const response = await axios.get(
+          `https://tile.openstreetmap.org/${tileCoord[0]}/${tileCoord[1]}/${tileCoord[2]}.png`,
+          {
+            responseType: "arraybuffer",
+          }
+        );
+
+        return (
+          "data:image/[format]; base64," +
+          Buffer.from(response.data, "binary").toString("base64")
+        );
+      } catch (e) {
+        console.log(e);
+        throw new Meteor.Error("tile-error", "failed to fetch tile");
+      }
+    },
+  });
+};
