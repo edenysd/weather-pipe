16ac6af9e10e238d1d192e0a40d5d142c7cc3be2
Edenys Deniz González <holofist1@gmail.com>
2024-03-12 11:59:05 -0400

    feat: setup layer selection

--- imports/ui/pages/Home/WeatherMap/WeatherMapCard.tsx
+++ imports/ui/pages/Home/WeatherMap/WeatherMapCard.tsx
@@ -1,11 +1,35 @@
 import * as Card from "~/components/card";
 import { cx } from "~/lib/create-style-context";
 import { OLMapDashboard } from "./OLMapWeather";
-import { Button } from "~/components/button";
 import { createSignal } from "solid-js";
+import * as Select from "~/components/select";
+import { FaSolidCheck, FaSolidUpDown } from "solid-icons/fa";
+
+const LAYER_OPTIONS = [
+  {
+    value: "temp_new",
+    label: "Temperature",
+  },
+  {
+    value: "wind_new",
+    label: "Wind speed",
+  },
+  {
+    value: "clouds_new",
+    label: "Clouds",
+  },
+  {
+    value: "precipitation_new",
+    label: "Precipitation",
+  },
+  {
+    value: "pressure_new",
+    label: "Sea level pressure",
+  },
+];
 
 export const WeatherMapCard = () => {
-  const [layer, setLayer] = createSignal("");
+  const [layer, setLayer] = createSignal("temp_new");
   return (
     <Card.Root
       class={cx(
@@ -13,8 +37,37 @@ export const WeatherMapCard = () => {
       )}
       maxW="full"
     >
-      <Card.Header>
-        <Card.Title>Add new location</Card.Title>
+      <Card.Header class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+        <Select.Root
+          positioning={{ sameWidth: true }}
+          items={LAYER_OPTIONS}
+          value={[layer()]}
+          onValueChange={(e) => {
+            setLayer(e.value[0]);
+          }}
+        >
+          <Select.Label>Layer</Select.Label>
+          <Select.Control>
+            <Select.Trigger>
+              <Select.ValueText placeholder="Select a layer" />
+              <FaSolidUpDown />
+            </Select.Trigger>
+          </Select.Control>
+          <Select.Positioner>
+            <Select.Content>
+              <Select.ItemGroup id="layer">
+                {LAYER_OPTIONS.map((item) => (
+                  <Select.Item item={item}>
+                    <Select.ItemText>{item.label}</Select.ItemText>
+                    <Select.ItemIndicator>
+                      <FaSolidCheck />
+                    </Select.ItemIndicator>
+                  </Select.Item>
+                ))}
+              </Select.ItemGroup>
+            </Select.Content>
+          </Select.Positioner>
+        </Select.Root>
       </Card.Header>
       <Card.Body class="w-full h-full grid grid-cols-1 p-0">
         <div class="h-full">
