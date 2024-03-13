6202bbaca761d8a2c929ddb0f503730a962a5d32
Edenys Deniz González <holofist1@gmail.com>
2024-03-13 04:35:35 -0400

    feat: setup data fetching from OW API

--- imports/api/collections/LocationsData.ts
+++ imports/api/collections/LocationsData.ts
@@ -11,3 +11,49 @@ LocationsData.schema = new SimpleSchema({
   forecast: { type: Object },
   lastUpdate: { type: Date },
 });
+
+export async function getLocationCurrentWeather({ lat, lng }) {
+  const currentWeatherResponse = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${Meteor.settings.OW_API_KEY}&units=metric`
+  );
+  return currentWeatherResponse.data;
+}
+
+export async function getLocationForecastWeather({ lat, lng }) {
+  const forecastWeatherResponse = await axios.get(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${Meteor.settings.OW_API_KEY}&units=metric`
+  );
+  return forecastWeatherResponse.data;
+}
+
+export async function updateLocationData({ lat, lng }) {
+  const currentWeather = await getLocationCurrentWeather({ lat, lng });
+  const forecastWeather = await getLocationForecastWeather({ lat, lng });
+
+  const storedLocation = LocationsData.findOne({
+    lat,
+    lng,
+  });
+
+  if (!storedLocation) {
+    return await LocationsData.insertAsync({
+      lat,
+      lng,
+      current: currentWeather,
+      forecast: forecastWeather,
+      lastUpdate: new Date(),
+    });
+  } else {
+    LocationsData.update(
+      { _id: storedLocation._id },
+      {
+        lat,
+        lng,
+        current: currentWeather,
+        forecast: forecastWeather,
+        lastUpdate: new Date(),
+      }
+    );
+    return storedLocation._id;
+  }
+}
