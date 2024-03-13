2389bd6fad5f61dc6286d3db3494fab8ab1b0da4
Edenys Deniz González <holofist1@gmail.com>
2024-03-13 04:38:06 -0400

    feat: add method add-location

---
+++ imports/api/methods/locations.ts
@@ -0,0 +1,40 @@
+import { check } from "meteor/check";
+import { UserPreferences } from "../collections/UserPreferences";
+import { Meteor } from "meteor/meteor";
+import { updateLocationData } from "../collections/LocationsData";
+
+export const startMethodAddLocation = () => {
+  Meteor.methods({
+    async ["add-location"]({ name, lat, lng }) {
+      if (!this.userId) {
+        throw new Meteor.Error(
+          "not-permited",
+          "Please log in to call this method"
+        );
+      }
+
+      check(name, String);
+      check(lat, Number);
+      check(lng, Number);
+
+      try {
+        const locationId = await updateLocationData({ lat, lng });
+        const draftUserPreference = {
+          userId: this.userId,
+          name,
+          lat,
+          lng,
+          locationId,
+        };
+
+        UserPreferences.insert(draftUserPreference);
+      } catch (e) {
+        console.error(e);
+        throw new Meteor.Error(
+          "add-location-error",
+          "failed to add new location"
+        );
+      }
+    },
+  });
+};
