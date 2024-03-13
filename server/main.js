a03b46df2b6a78100a622cdcdee48262ea76efac
Edenys Deniz González <holofist1@gmail.com>
2024-03-13 04:40:44 -0400

    feat: clear entrypoint and run all the code in the fiber

--- server/main.js
+++ server/main.js
@@ -1,44 +1,16 @@
 import { Meteor } from "meteor/meteor";
-import { LinksCollection } from "/imports/api/links";
+import "meteor/aldeed:collection2/static";
+import "/imports/api/collections/attachSchemas";
 import { startMethodMap } from "/imports/api/methods/map";
+import { startMethodAddLocation } from "/imports/api/methods/locations";
 import { createOWTimer } from "/imports/api/timers/OWTimer";
-import { publishDashboard } from "/imports/api/publications/example";
-
-async function insertLink({ title, url }) {
-  await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
-}
+import { publishLocationsDashboard } from "/imports/api/publications/locationsData";
+import { publishUserPreferencesDashboard } from "/imports/api/publications/userPreferences";
 
 Meteor.startup(async () => {
-  // If the Links collection is empty, add some data.
-  if ((await LinksCollection.find().countAsync()) === 0) {
-    await insertLink({
-      title: "Do the Tutorial",
-      url: "https://www.solidjs.com/tutorial/introduction_basics",
-    });
-
-    await insertLink({
-      title: "Follow the Guide",
-      url: "https://guide.meteor.com",
-    });
-
-    await insertLink({
-      title: "Read the Docs",
-      url: "https://docs.meteor.com",
-    });
-
-    await insertLink({
-      title: "Discussions",
-      url: "https://forums.meteor.com",
-    });
-  }
-
-  // We publish the entire Links collection to all clients.
-  // In order to be fetched in real-time to the clients
-  Meteor.publish("links", function () {
-    return LinksCollection.find();
-  });
+  startMethodMap();
+  createOWTimer();
+  startMethodAddLocation();
+  publishUserPreferencesDashboard();
+  publishLocationsDashboard();
 });
-
-startMethodMap();
-createOWTimer();
-publishDashboard();
