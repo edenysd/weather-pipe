845d399063e55c2dc4b83fe824ca4d06ad0a5deb
Edenys Deniz González <holofist1@gmail.com>
2024-03-13 04:41:12 -0400

    feat: prepare subscription skeleton

--- imports/ui/pages/Home/Dashboard/Dashboard.tsx
+++ imports/ui/pages/Home/Dashboard/Dashboard.tsx
@@ -1,10 +1,34 @@
-import { createSignal } from "solid-js";
+import { createSignal, onCleanup } from "solid-js";
 import { Button } from "~/components/button";
 import { CreateNewLocationCard } from "./CreateNewLocationCard";
+import { Tracker } from "meteor/tracker";
+import { LocationsData } from "../../../../api/collections/LocationsData";
+import { UserPreferences } from "../../../../api/collections/UserPreferences";
 
 export const Dashboard = (props) => {
   const [draftNewLocation, setDraftNewLocation] = createSignal(false);
 
+  const locationsSubscription = Meteor.subscribe("dashboard-locations");
+  const preferencesSubscription = Meteor.subscribe(
+    "dashboard-user-preferences"
+  );
+
+  const [isReady, setIsReady] = createSignal(
+    locationsSubscription.ready() && preferencesSubscription.ready()
+  );
+
+  const [links, setLinks] = createSignal([]);
+
+  Tracker.autorun(async () => {
+    setIsReady(
+      locationsSubscription.ready() && preferencesSubscription.ready()
+    );
+    console.log(LocationsData.find().fetch());
+    console.log(UserPreferences.find().fetch());
+  });
+  onCleanup(() => {
+    locationsSubscription.stop();
+  });
   return (
     <div class="w-full h-full flex flex-col items-center p-8 gap-8">
       <section class="flex flex-col items-center gap-4">
