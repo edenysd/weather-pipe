a5b99b4bbe06d4f2db2ccb5c0558c0dea8dbd0f3
Edenys Deniz González <holofist1@gmail.com>
2024-03-14 04:19:30 -0400

    feat: add menus to remove and swap positions

--- imports/ui/pages/Home/Dashboard/LocationCard.tsx
+++ imports/ui/pages/Home/Dashboard/LocationCard.tsx
@@ -1,6 +1,10 @@
 import { Index, createEffect, createSignal } from "solid-js";
 import * as Card from "~/components/card";
 import { ForecastElement } from "./ForecastElement";
+import * as Menu from "~/components/menu";
+import { TbDotsVertical, TbTrashFilled } from "solid-icons/tb";
+import { FaSolidAnglesLeft, FaSolidAnglesRight } from "solid-icons/fa";
+import { cx } from "~/lib/create-style-context";
 
 const hourTimeFormatter = new Intl.DateTimeFormat("en", {
   timeStyle: "short",
@@ -12,7 +16,13 @@ const shortTimeFormatter = new Intl.DateTimeFormat("en", {
   day: "numeric",
 });
 
-export const LocationCard = ({ preference }) => {
+export const LocationCard = ({
+  preference,
+  handleMove,
+  handleRemove,
+  isFirst,
+  isLast,
+}) => {
   const [forecastedList, setForecastedList] = createSignal([]);
 
   createEffect(() => {
@@ -32,11 +42,56 @@ export const LocationCard = ({ preference }) => {
 
     setForecastedList(forecastDays);
   });
-
+  console.log(isFirst, isLast);
   return (
     <Card.Root maxW="full" class="shadow-sm border-[1px]">
-      <Card.Header class="p-4">
-        <Card.Title>{preference().name}</Card.Title>
+      <Card.Header class="flex flex-row justify-between p-4">
+        <Card.Title class="w-fit">{preference().name}</Card.Title>
+        <Menu.Root>
+          <Menu.Trigger class="aspect-square w-6">
+            <TbDotsVertical class="w-full h-full" />
+          </Menu.Trigger>
+          <Menu.Positioner>
+            <Menu.Content class="w-full">
+              <Menu.ItemGroup class="w-full" id="group-1">
+                <Menu.Item
+                  class="flex flex-row gap-2 text-base"
+                  id="remove-location"
+                  onClick={handleRemove}
+                >
+                  <TbTrashFilled />
+                  Remove location
+                </Menu.Item>
+                <Menu.Item
+                  class={cx(
+                    "flex flex-row gap-2 text-base",
+                    isFirst
+                      ? "pointer-events-none text-gray-400 fill-gray-400"
+                      : ""
+                  )}
+                  id="move-left"
+                  onClick={async () => handleMove(+1)}
+                >
+                  <FaSolidAnglesLeft class="fill-inherit" />
+                  Move Left
+                </Menu.Item>
+                <Menu.Item
+                  class={cx(
+                    "flex flex-row gap-2 text-base",
+                    isLast
+                      ? "pointer-events-none text-gray-400 fill-gray-400"
+                      : ""
+                  )}
+                  id="move-rigth"
+                  onClick={() => handleMove(-1)}
+                >
+                  <FaSolidAnglesRight class="fill-inherit" />
+                  Move Right
+                </Menu.Item>
+              </Menu.ItemGroup>
+            </Menu.Content>
+          </Menu.Positioner>
+        </Menu.Root>
       </Card.Header>
       <Card.Body class="flex flex-col gap-4 p-4">
         <div class="flex flex-col items-center w-full">
