b3239d8a7ab0e71d7ba7c9d6444a10782e23a148
Edenys Deniz González <holofist1@gmail.com>
2024-03-14 03:07:58 -0400

    feat: add logout icon

--- imports/ui/pages/Home/NavBar.tsx
+++ imports/ui/pages/Home/NavBar.tsx
@@ -3,6 +3,7 @@ import { SideBarToggle } from "./SideBar";
 import * as Menu from "~/components/menu";
 import { useNavigate } from "@solidjs/router";
 import { logOut } from "~/lib/auth";
+import { TbLogout } from "solid-icons/tb";
 
 export const NavBar = () => {
   const navigate = useNavigate();
@@ -16,15 +17,16 @@ export const NavBar = () => {
               <Avatar />
             </Menu.Trigger>
             <Menu.Positioner>
-              <Menu.Content class="w-full">
+              <Menu.Content class="w-full min-w-44">
                 <Menu.ItemGroup id="group-1">
-                  <Menu.ItemGroupLabel for="group-1">
+                  <Menu.ItemGroupLabel class="text-base" for="group-1">
                     <div>Welcome!</div>
                     <div class="font-light">{Meteor.user()?.username}</div>
                   </Menu.ItemGroupLabel>
 
                   <Menu.Separator />
                   <Menu.Item
+                    class="flex flex-row gap-4 text-base"
                     id="profile"
                     onClick={async () => {
                       logOut().then(() => {
@@ -32,6 +34,7 @@ export const NavBar = () => {
                       });
                     }}
                   >
+                    <TbLogout class="h-6 w-6" />
                     Logout
                   </Menu.Item>
                 </Menu.ItemGroup>
